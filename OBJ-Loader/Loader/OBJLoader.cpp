a6c816060895595e1cef8644f3b8cf7de64db3d2
Iwein Bau <iwein.bau@gmail.com>
2019-02-21 15:59:41 +0100

    Parsing vetex positions and vertex normals.

--- OBJ-Loader/Loader/OBJLoader.cpp
+++ OBJ-Loader/Loader/OBJLoader.cpp
@@ -6,49 +6,86 @@
 //  Copyright © 2019 Iwein Bau. All rights reserved.
 //
 
-#include "MeshLoader.hpp"
-#include "../Mesh/Mesh.hpp"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-//Constructor
-MeshLoader::OBJLoader::OBJLoader():MeshLoader(){}
-//Destructor
-MeshLoader::OBJLoader::~OBJLoader(){};
+#include "MeshLoader.hpp"
+#include "../Mesh/Mesh.hpp"
+#include "../Utils/Vect3.hpp"
+#include "../Utils/Algorithm.hpp"
 
-//OBJ Loader object.
-bool MeshLoader::OBJLoader::loadMesh (std::string filePath){
-    // If the file is not an .obj file return false
-    if (filePath.substr(filePath.size() - 4, 4) != ".obj"){
-        std::cout << "No .obj file found at given file location: "<<filePath << std::endl;
+namespace MeshLoader {
+    //Constructor
+    OBJLoader::OBJLoader():MeshLoader(){}
+    //Destructor
+    OBJLoader::~OBJLoader(){};
+    
+    //OBJ Loader object.
+    bool OBJLoader::loadMesh (std::string filePath){
+        // If the file is not an .obj file return false
+        if (filePath.substr(filePath.size() - 4, 4) != ".obj"){
+            std::cout << "No .obj file found at given file location: "<<filePath << std::endl;
+            
+        }
+        //Open file stream
+        std::ifstream file(filePath);
+        
+        //check if file is open.
+        if (!file.is_open()){
+            std::cout << "File was not opened!" << std::endl;
+            return false;
+        }
+        
+        //Do file loading.
+        std::cout << "Parsing obj-file: "<<filePath << std::endl;
+        
+        
+        //constuct mesh data.
+        std::vector<Vect3> Positions;
+        std::vector<Vect3> Normals;
+        std::vector<unsigned int> Indices;
 
+        
+
+        
+        //the current line
+        std::string currentLine;
+        //loop over each line and parse the needed data.
+        while(std::getline(file, currentLine)){
+            //for now we just print the line
+            std::cout << currentLine << std::endl;
+            
+            //check if the line starts with v -> vertex.
+            if(algorithm::startsWith(currentLine, "v ")){
+                //construct new vertex position.
+                std::vector<std::string> line_split = algorithm::split(currentLine,' ');
+                
+                float x = std::stof(line_split[1]);
+                float y = std::stof(line_split[2]);
+                float z = std::stof(line_split[3]);
+                Vect3 pos = Vect3(x,y,z);
+                Positions.push_back(pos);
+            }
+            
+            //check if the line starts with v -> vertex.
+            if(algorithm::startsWith(currentLine, "vn ")){
+                //construct new vertex position.
+                std::vector<std::string> line_split = algorithm::split(currentLine,' ');
+                
+                float x = std::stof(line_split[1]);
+                float y = std::stof(line_split[2]);
+                float z = std::stof(line_split[3]);
+                Vect3 normal = Vect3(x,y,z);
+                Normals.push_back(normal);
+            }
+        }
+        
+        //Load mesh data.
+        
+        //close stream
+        //return new mesh.
+        return true;
     }
-    //Do file loading.
-    std::cout << "Parsing obj-file: "<<filePath << std::endl;
-    
-    //Open file stream
-    std::ifstream file(filePath);
-    
-    //check if file is open.
-    if (!file.is_open()){
-        std::cout << "File was not opened!" << std::endl;
-        return false;
-    }
-    
-    //constuct mesh data.
-    
-    
-    //the current line
-    std::string currentLine;
-    //loop over each line and parse the needed data.
-    while(std::getline(file, currentLine)){
-        //for now we just print the line
-        std::cout << currentLine << std::endl;
-    }
-    
-    //Load mesh data.
-    
-    //close stream
-    //return new mesh.
-    return true;
+
 }
---
+++ OBJ-Loader/Utils/Vect3.hpp
@@ -0,0 +1,81 @@
+#ifndef vector3f
+#define vector3f
+
+#include "math.h"
+#include <iostream>
+#include <algorithm>
+
+class Vect3 {
+public:
+    double x_;
+    double y_;
+    double z_;
+    
+    Vect3():x_(0),y_(0),z_(0)
+    {}
+    
+    Vect3(double x, double y, double z):x_(x),y_(y),z_(z)
+    {}
+    
+    Vect3(Vect3 const &v):x_(v.x_),y_(v.y_),z_(v.z_)
+    {}
+    
+    ~Vect3(){}
+    Vect3& operator= (Vect3 const& v){
+        x_ = v.x_; y_ = v.y_; z_=v.z_;
+        return(*this);
+    }
+    
+    double length() {
+        return (sqrt(x_ * x_ + y_ * y_ + z_ * z_));
+    }
+    
+    Vect3 normalize() {
+        double mag = length();
+        return Vect3(x_ /mag, y_ / mag, z_ /mag);
+    }
+    
+    Vect3 neg() const{
+        return Vect3(-x_,-y_,-z_);
+    }
+    
+    double dot(Vect3 const& other) const {
+        return(x_ * other.x_ + y_ * other.y_ + z_ * other.z_);
+    }
+    
+    Vect3 cross(Vect3 other) {
+        double x = y_ * other.z_ - z_ * other.y_;
+        double y = z_ * other.x_ - x_ * other.z_;
+        double z = x_ * other.y_ - y_ * other.x_;
+        
+        return Vect3(x, y, z);
+    }
+    
+    void print()
+    {
+        std::cout << x_ << '/' << y_ << '/' << z_<< std::endl;
+    }
+};
+
+Vect3 operator + (const Vect3 & v1, const Vect3 &v2) {
+    return Vect3(v1.x_ + v2.x_, v1.y_ + v2.y_, v1.z_ + v2.z_);
+}
+
+Vect3 operator - (const Vect3 & v1, const Vect3 &v2){
+    return Vect3(v1.x_ - v2.x_, v1.y_ - v2.y_, v1.z_ - v2.z_);
+}
+
+Vect3 operator * (const Vect3 & v1, const Vect3 &v2){
+    return Vect3(v1.x_ * v2.x_, v1.y_ * v2.y_,v1.z_ * v2.z_);
+}
+
+Vect3 operator / (const Vect3 & v1, double const& s) {
+    return Vect3(v1.x_ / s, v1.y_ / s,v1.z_ / s);
+}
+
+
+Vect3 operator * (const Vect3 & v1, double const& s) {
+    return Vect3(v1.x_ * s, v1.y_ * s,v1.z_ * s);
+}
+
+#endif
